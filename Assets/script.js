10d12fbd4f014723fe2ac91d11c0ea78cefcc209
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-07-23 16:04:56 -0400

    Created function to build rows

--- Assets/script.js
+++ Assets/script.js
@@ -4,38 +4,18 @@ $(document).ready(function() {
     // To set the date in header
     const curDayEl = $('#currentDay');
     const todaysDate = moment();
-    console.log(moment().day()); //To remove later
     // The container that holds the calendar
     const container = $('div.container');
-
 
     // To display the date in the header - using moment.js
     curDayEl.text(todaysDate.format('dddd, MMMM DD YYYY'));
 
-    // Iterate through the hours to populate the rows of the calendar
-    for (i = 9; i < 18; i++) {
-        // container.html($('<div class="row time-block"></div>'));
-        const rowEl = $('<div>');
-        const hourEl = $('<div>');
-        const calTextArea = $('<textarea>');
-        const saveBtn = $('<button>');
+    // To populate the rows of the calendar
+    createRows(9, 18);
 
-        rowEl.addClass('row time-block');
-        hourEl.addClass('col-2 col-md-1 hour');
 
-        //To set the hour in first column - use moment.js for this?
-        hourEl.text(setHour(i));
 
-        calTextArea.addClass('col-8 col-sm-10');
-        calTextArea.attr('name', 'hourplan');
-        saveBtn.addClass('col-2 col-md-1 saveBtn');
-        saveBtn.attr('type', 'button');
-        saveBtn.html('<i class="far fa-save"></i>');
-
-        container.append(rowEl);
-        rowEl.append(hourEl, calTextArea, saveBtn);
-    }
-
+    // Should these functions go at the top?
     // Takes an hour from 24 hour time to make it AM or PM
     function setHour(hr) {
         let time = 0;
@@ -49,6 +29,48 @@ $(document).ready(function() {
         }
 
         return time;
-    }
+    };
+
+    function createRows(start, end) {
+        // Iterate through the hours (from start to end - end non-inclusive) to populate the rows of the calendar
+        for (i = start; i < end; i++) {
+            const rowEl = $('<div>');
+            const hourEl = $('<div>');
+            const calTextArea = $('<textarea>');
+            const saveBtn = $('<button>');
+
+            rowEl.addClass('row time-block');
+            hourEl.addClass('col-2 col-md-1 hour');
+
+            //To set the hour in first column - use moment.js for this?
+            hourEl.text(setHour(i));
+
+            calTextArea.addClass('col-8 col-sm-10');
+            calTextArea.attr({
+                name: 'hourplan',
+                'data-time': i
+            });
+            saveBtn.addClass('col-2 col-md-1 saveBtn');
+            saveBtn.attr({
+                type: 'button',
+                'data-time': i
+            });
+            saveBtn.html('<i class="far fa-save"></i>');
+
+            container.append(rowEl);
+            rowEl.append(hourEl, calTextArea, saveBtn);
+        }
+    };
+
+    // Add event listener for save button - listen for all .saveBtn
+    container.on("click", "button", function(event) {
+       // event.preventDefault(); DO I NEED THIS?
+        const thisBtn = this;
+        console.log(event);
+        console.log(thisBtn);
+        console.log(thisBtn.getAttribute('data-time'));
+        console.log($(this).parent());
+    });
+
 
 });
