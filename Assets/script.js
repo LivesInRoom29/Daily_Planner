286f9d34864e3c5b1a89f1b14bc6d00d6cd82619
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-07-23 16:08:20 -0400

    Created calednar in HTML instead of JS

--- Assets/script.js
+++ Assets/script.js
@@ -13,28 +13,28 @@ $(document).ready(function() {
     curDayEl.text(todaysDate.format('dddd, MMMM DD YYYY'));
 
     // Iterate through the hours to populate the rows of the calendar
-    for (i = 9; i < 18; i++) {
-        // container.html($('<div class="row time-block"></div>'));
-        const rowEl = $('<div>');
-        const hourEl = $('<div>');
-        const calTextArea = $('<textarea>');
-        const saveBtn = $('<button>');
+    // for (i = 9; i < 18; i++) {
+    //     // container.html($('<div class="row time-block"></div>'));
+    //     const rowEl = $('<div>');
+    //     const hourEl = $('<div>');
+    //     const calTextArea = $('<textarea>');
+    //     const saveBtn = $('<button>');
 
-        rowEl.addClass('row time-block');
-        hourEl.addClass('col-2 col-md-1 hour');
+    //     rowEl.addClass('row time-block');
+    //     hourEl.addClass('col-2 col-md-1 hour');
 
-        //To set the hour in first column - use moment.js for this?
-        hourEl.text(setHour(i));
+    //     //To set the hour in first column - use moment.js for this?
+    //     hourEl.text(setHour(i));
 
-        calTextArea.addClass('col-8 col-sm-10');
-        calTextArea.attr('name', 'hourplan');
-        saveBtn.addClass('col-2 col-md-1 saveBtn');
-        saveBtn.attr('type', 'button');
-        saveBtn.html('<i class="far fa-save"></i>');
+    //     calTextArea.addClass('col-8 col-sm-10');
+    //     calTextArea.attr('name', 'hourplan');
+    //     saveBtn.addClass('col-2 col-md-1 saveBtn');
+    //     saveBtn.attr('type', 'button');
+    //     saveBtn.html('<i class="far fa-save"></i>');
 
-        container.append(rowEl);
-        rowEl.append(hourEl, calTextArea, saveBtn);
-    }
+    //     container.append(rowEl);
+    //     rowEl.append(hourEl, calTextArea, saveBtn);
+    // }
 
     // Takes an hour from 24 hour time to make it AM or PM
     function setHour(hr) {
