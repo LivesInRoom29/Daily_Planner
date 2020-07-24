87278dafee6ec582db3fbd89f304411ba888484e
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-07-23 20:05:51 -0400

    Add functionality to save txt & disp it on pg load

--- Assets/script.js
+++ Assets/script.js
@@ -7,13 +7,26 @@ $(document).ready(function() {
     // The container that holds the calendar
     const container = $('div.container');
 
+&    let planner = JSON.parse(localStorage.getItem('plannerKEY')) || [
+        {time: "9", text: ""},
+        {time: "10", text: ""},
+        {time: "11", text: ""},
+        {time: "12", text: ""},
+        {time: "13", text: ""},
+        {time: "14", text: ""},
+        {time: "15", text: ""},
+        {time: "16", text: ""},
+        {time: "17", text: ""},
+    ];
+
     // To display the date in the header - using moment.js
     curDayEl.text(todaysDate.format('dddd, MMMM DD YYYY'));
 
     // To populate the rows of the calendar
     createRows(9, 18);
 
-
+    // Upon page load, this function will fill the planner with saved plans, from 9pm (9:00) - 5pm (18:00)
+    getSavedPlans(9,18);
 
     // Should these functions go at the top?
     // Takes an hour from 24 hour time to make it AM or PM
@@ -31,6 +44,17 @@ $(document).ready(function() {
         return time;
     };
 
+    function getSavedPlans(start, end) {
+        const range = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+        //Loop through the array to add text to the planner where it exists
+        range.forEach(function(element) {
+            const textblock = $(`.text${element}`);
+            const index = element - 9;
+            textblock.append(planner[index].text);
+        });
+    };
+
     function createRows(start, end) {
         // Iterate through the hours (from start to end - end non-inclusive) to populate the rows of the calendar
         for (i = start; i < end; i++) {
@@ -45,12 +69,12 @@ $(document).ready(function() {
             //To set the hour in first column - use moment.js for this?
             hourEl.text(setHour(i));
 
-            calTextArea.addClass('col-8 col-sm-10');
+            calTextArea.addClass(`col-8 col-sm-10 text${i}`);
             calTextArea.attr({
                 name: 'hourplan',
                 'data-time': i
             });
-            saveBtn.addClass('col-2 col-md-1 saveBtn');
+            saveBtn.addClass('col-2 col-md-1 saveBtn ' + i);
             saveBtn.attr({
                 type: 'button',
                 'data-time': i
@@ -62,14 +86,32 @@ $(document).ready(function() {
         }
     };
 
+    function saveText(timeblock) {
+        const thisTextArea = $(`.text${timeblock}`);
+        const text = thisTextArea.val();
+        const index = timeblock - 9
+        // Find correct time, edit the text in the object
+        planner[index].text = text;
+        // Save to memory
+        localStorage.setItem('plannerKEY', JSON.stringify(planner));
+    };
+
     // Add event listener for save button - listen for all .saveBtn
     container.on("click", "button", function(event) {
        // event.preventDefault(); DO I NEED THIS?
-        const thisBtn = this;
+        const thisBtn = $(this);
+        const classes = thisBtn.attr('class').split(' ');
+        const lastClass = classes[classes.length - 1];
+
+        const textArea = $(`.text${lastClass}`);
+        const textValue = textArea.val();
+        console.log(textValue);
+
         console.log(event);
         console.log(thisBtn);
-        console.log(thisBtn.getAttribute('data-time'));
+        console.log(thisBtn.attr('class'));
         console.log($(this).parent());
+        saveText(lastClass);
     });
 
 
